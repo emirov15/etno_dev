c7b368fc85c64129d5980ef3fc2d31da90710b4c
emirov15 <samatemir2@gmail.com>
2024-07-03 15:06:33 +0600

    header buttu

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import './App.scss';
 import Header from './components/Header';
 import { Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Vacancies from './components/Vacancies';
 
 function App() {
-  return (
-		<div className=''>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Hero />} />
-				{/* <Route path='/' element={<Hero />} /> */}
-			</Routes>
-		</div>
-	)
+    return (
+			<div className=''>
+				<Header />
+				<Footer />
+				<Routes>
+					<Route path='/' element={<Hero />} />
+					<Route path='/Vacancies' element={<Vacancies />} />
+				</Routes>
+			</div>
+		)
 }
 
 export default App;
---
+++ src/components/Vacancies/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Vacancies = () => {
+    return (
+        <div>
+            emir1
+        </div>
+    );
+};
+
+export default Vacancies;
