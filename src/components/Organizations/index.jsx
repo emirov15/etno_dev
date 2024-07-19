f7c3f80f47805b5ac177f0cfeb1980dd467fe661
Elzar <chynarbekov07@gmail.com>
2024-07-19 18:31:08 +0600

    Change complete, do git pull

--- src/components/Organizations/index.jsx
+++ src/components/Organizations/index.jsx
@@ -5,7 +5,7 @@ const Organizations = () => {
 				<div className='organizations'>
 					<div className='organizations-content'>
 						<div className='information-organization'>
-							<div className='jobs-item-field-company'>
+							<div className='organization-jobs-item-field-company'>
 								<img  className='organization-img'
 									src='https://upload.wikimedia.org/wikipedia/commons/7/7a/BeeLine_logo.png'
 									alt='img'
@@ -44,7 +44,7 @@ const Organizations = () => {
 					</div>
 					<div className='organizations-content'>
 						<div className='information-organization'>
-							<div className='jobs-item-field-company'>
+							<div className='organization-jobs-item-field-company'>
 								<img  className='organization-img'
 									src='https://upload.wikimedia.org/wikipedia/commons/7/7a/BeeLine_logo.png'
 									alt='img'
@@ -81,84 +81,7 @@ const Organizations = () => {
 							</div>
 						</div>
 					</div>
-					<div className='organizations-content'>
-						<div className='information-organization'>
-							<div className='jobs-item-field-company'>
-								<img className='organization-img'
-									src='https://upload.wikimedia.org/wikipedia/commons/7/7a/BeeLine_logo.png'
-									alt='img'
-								/>
-								<h4>
-									<span>
-										Компания <br />
-									</span>
-									Билайн
-								</h4>
-							</div>
-							<div className='vacancies-event-video'>
-								<div className='jobs-item-field-vacancies'>
-									<h4>
-										<span>Вакансии</span>
-										<br />
-										100
-									</h4>
-								</div>
-								<div className='jobs-item-field-event'>
-									<h4>
-										<span>Мероприятие</span>
-										<br />
-										350
-									</h4>
-								</div>
-								<div className='jobs-item-field-video'>
-									<h4>
-										<span>Видео</span>
-										<br />
-										10
-									</h4>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className='organizations-content'>
-						<div className='information-organization'>
-							<div className='jobs-item-field-company'>
-								<img className='organization-img'
-									src='https://upload.wikimedia.org/wikipedia/commons/7/7a/BeeLine_logo.png'
-									alt='img'
-								/>
-								<h4>
-									<span>
-										Компания <br />
-									</span>
-									Билайн
-								</h4>
-							</div>
-							<div className='vacancies-event-video'>
-								<div className='jobs-item-field-vacancies'>
-									<h4>
-										<span>Вакансии</span>
-										<br />
-										100
-									</h4>
-								</div>
-								<div className='jobs-item-field-event'>
-									<h4>
-										<span>Мероприятие</span>
-										<br />
-										350
-									</h4>
-								</div>
-								<div className='jobs-item-field-video'>
-									<h4>
-										<span>Видео</span>
-										<br />
-										10
-									</h4>
-								</div>
-							</div>
-						</div>
-					</div>
+				
 					<button className='next-page	'>Следующая страница</button>
 				</div>
 			</div>
