b9d6473b86c1a19e88a9d99b10d7e72d6d5b81a1
emirov15 <samatemir2@gmail.com>
2024-07-31 13:56:45 +0600

    Events integration jasoo!

--- src/components/Events/EventsList.tsx
+++ src/components/Events/EventsList.tsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-// import event from '../../assets/img/eve';
+import { Link } from 'react-router-dom'
 
 const EventsList = ({
 	location = '',
@@ -14,9 +13,8 @@ const EventsList = ({
 }) => {
 	const date1 = new Date(date)
 
-	// Extract year, month, and day
 	const year = date1.getFullYear()
-	const month = date1.toLocaleString('default', { month: 'long' }) // Months are 0-based, so add 1
+	const month = date1.toLocaleString('default', { month: 'long' })
 	const day = date1.getDate()
 	const hours = date1.getHours()
 	const minutes = date1.getMinutes()
@@ -24,34 +22,36 @@ const EventsList = ({
 	const formattedMinutes = minutes.toString().padStart(2, '0')
 
 	return (
-		<div className='events'>
-			<div className='events--foto'>
-				<Link to={'/detalmerop'}>
-					{' '}
-					{/* { <img src={event} alt='img' /> } */}
-				</Link>
-				<div className='events--foto__text'>
-					<h4>
-						{day} {month} {year}{' '}
-						<span>
-							{formattedHours}:{formattedMinutes}
-						</span>
-					</h4>
-					<h1>{name}</h1>
+		<Link to={'/eventDetals'}>
+			<div className='events'>
+				<div className='events--foto'>
+					<img
+						src='https://devkg.com/images/events/e975ab09a375897590241aaec11d51fa.png'
+						data-v-17b3db24='img'
+					/>
+					<div className='events--foto__text'>
+						<h4>
+							{day} {month} {year}{' '}
+							<span>
+								{formattedHours}:{formattedMinutes}
+							</span>
+						</h4>
+						<h1>{name}</h1>
 
-					<div className='events--foto__text--par'>
-						<div className='events--foto__text--par__kor1'>
-							<h6>Организатор</h6>
-							<h2>{organization_name}</h2>
-						</div>
-						<div className='events--foto__text--par__kor2'>
-							<h6>Когда</h6>
-							<h2>{location}</h2>
+						<div className='events--foto__text--par'>
+							<div className='events--foto__text--par__kor1'>
+								<h6>Организатор</h6>
+								<h2>{organization_name}</h2>
+							</div>
+							<div className='events--foto__text--par__kor2'>
+								<h6>Когда</h6>
+								<h2>{location}</h2>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
 
